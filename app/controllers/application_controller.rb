a45d4accacfcc5bc6d65898456ac3f141f26e7cc
Pablo <pablochavezing@gmail.com>
2021-06-07 19:04:37 -0700

    Mejorando las vistas de devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+#Configuracion para guardar toda la base de datos que queramos de devise
+#Ejecutar este metodo solo si se inicio el controlador de devise
+before_action :configure_devise_params, if: :devise_controller?
+
+    def configure_devise_params
+        devise_parameter_sanitizer.permit(:sign_up) do |user|
+            user.permit(:name, :email, :url_photo, :password, :password_confirmation)
+        end
+    end
 end
+
